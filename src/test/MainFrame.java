da66f5fe744add1d9ae1862120baf7691e97238f
“goodboy” <88391769+40273922@users.noreply.github.com>
2023-04-29 01:05:09 +0800

    huge change

--- src/test/MainFrame.java
+++ src/test/MainFrame.java
@@ -1,10 +1,13 @@
 package test;
 
-import com.formdev.flatlaf.FlatDarculaLaf;
-import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.intellijthemes.FlatCarbonIJTheme;
 
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -13,6 +16,26 @@ import java.net.URISyntaxException;
  * @author Zhai Jinpei
  */
 public class MainFrame extends JFrame{
+    static class Circle{
+        static final double PI = Math.PI;
+        static final double R = 1;
+        static final double C = 2 * PI * R;
+        double N = 1;
+        double dR = R / N;
+        double dS = 2 * PI * dR * dR;
+        double dC = 2 * PI * dR;
+        double AREAR = dS * (1 + N) * N / 2;
+
+        Circle setN(double n){
+            N = n;
+            dR = R / N;
+            dC = 2 * PI * dR;
+            dS = 2 * PI * dR * dR;
+            AREAR = dS * (1 + N) * N / 2;
+            return this;
+        }
+    }
+
     Circle circle = new Circle();
 
     public static void main(String[] args){
@@ -22,21 +45,23 @@ public class MainFrame extends JFrame{
     public MainFrame(){
         SwingUtilities.invokeLater(()->{
             try{
-                UIManager.setLookAndFeel(new FlatIntelliJLaf());
+                UIManager.setLookAndFeel(new FlatCarbonIJTheme());
             }catch(UnsupportedLookAndFeelException e){
                 throw new RuntimeException(e);
             }
-            setTitle("定积分计算圆面积演示");
-            setSize(900,800);
+            setTitle("定积分计算圆面积小组项目演示");
+            setSize(Toolkit.getDefaultToolkit().getScreenSize());
             setVisible(true);
             setDefaultCloseOperation(EXIT_ON_CLOSE);
             JPanel circlePanel = new JPanel(){
                 @Override
                 protected void paintComponent(Graphics g){
                     super.paintComponent(g);
-                    for(int i = 0;i <= circle.N;i++){
-                        g.drawOval((int)(20 + circle.dR * 120 * i),(int)(20 + circle.dR * 120 * i),2 * (int)(100 - circle.dR * 120 * i),2 * (int)(100 - circle.dR * 120 * i));
-                    }
+                    g.setColor(Color.cyan);
+                    g.drawLine((int)(10 + 100 * 2.4),(int)(150 + 100 * 2.4),(int)(100 * 4.8 + 10),(int)(150 + 100 * 2.4));
+                    g.setColor(Color.red);
+                    for(int i = 0;i <= circle.N;i++)
+                        g.drawOval((int)(10 + circle.dR * 120 * i * 2.4),(int)(150 + circle.dR * 120 * i * 2.4),2 * (int)(100 * 2.4 - circle.dR * 120 * i * 2.4),2 * (int)(100 * 2.4 - circle.dR * 120 * i * 2.4));
                 }
             };
             JPanel argsPanel = new JPanel();
@@ -44,74 +69,86 @@ public class MainFrame extends JFrame{
                 @Override
                 protected void paintComponent(Graphics g){
                     super.paintComponent(g);
-                    g.drawLine(50,10,50,(int)Circle.C * 120 + 40);
-                    g.drawLine(50,(int)Circle.C * 120 + 10,170,(int)Circle.C * 120 + 10);
-                    g.drawLine(170,10,170,(int)Circle.C * 120 + 10);
+                    g.drawLine(50,30,50,(int)Circle.C * 120 + 50);
+                    g.drawLine(50,(int)Circle.C * 120 + 50,(int)(50 + 120 * 3.8),(int)Circle.C * 120 + 50);
+                    g.drawLine((int)(50 + 120 * 3.8),30,(int)(50 + 120 * 3.8),(int)Circle.C * 120 + 50);
+                    g.setColor(Color.cyan);
+                    g.drawLine(50,(int)Circle.C * 120 + 50,(int)(50 + 120 * 3.8),30);
                     for(int i = 0;i <= circle.N;i++){
-                        g.drawLine((int)(50 + circle.dR * i * 120),(int)(Circle.C * 120 + 10) - (int)(circle.dC * 120 * i),(int)(50 + circle.dR * (i+1) * 120),(int)(Circle.C * 120 + 10) - (int)(circle.dC * 120 * i));
-                        g.drawRect((int)(50 + circle.dR * i * 120),(int)(Circle.C * 120 + 10) - (int)(circle.dC * 120 * i),(int)circle.dR * 120,(int)circle.dC * 120 * i);
+                        if(i == 0) g.setColor(Color.red);
+                        else g.setColor(Color.WHITE);
+                        g.drawLine((int)(50 + circle.dR * i * 120 * 3.8),(int)(Circle.C * 120) - (int)(circle.dC * 120 * i) + 20,(int)(50 + circle.dR * (i + 1) * 120 * 3.8),(int)(Circle.C * 120) - (int)(circle.dC * 120 * i) + 20);
+                        g.drawRect((int)(50 + circle.dR * i * 120 * 3.8),(int)(Circle.C * 120) - (int)(circle.dC * 120 * i) + 20,(int)(circle.dR * 120 * 3.8),(int)(circle.dC * 120 * i) - 3);
                     }
-                    System.out.println("===========================");
                 }
             };
-            JLabel N = new JLabel("N=");
-            JTextField Nfield = new JTextField();
-            JLabel AREALabel = new JLabel("近似面积=");
+            JLabel N = new JLabel("请滑动N值标尺 或 按下END键(右方向键)=");
+            JSlider jSlider = new JSlider(0,800);
+            jSlider.setPaintTicks(true);
+            jSlider.setMajorTickSpacing(80);
+            jSlider.setMinorTickSpacing(8);
+            jSlider.setPaintTrack(true);
+            jSlider.setPaintLabels(true);
+            jSlider.setOrientation(SwingConstants.HORIZONTAL);
+            JLabel tip = new JLabel("重要参数列表");
+            JTextField textField = new JTextField("对参数求值的结果");
+            textField.setEditable(false);
+            JLabel NN = new JLabel("N =");
+            JTextField NNt = new JTextField();
+            NNt.setEditable(false);
+            JLabel AREALabel = new JLabel("积分后的近似面积=");
             JTextField AREA = new JTextField();
-            JLabel R = new JLabel("半径=");
+            AREA.setEditable(false);
+            JLabel R = new JLabel("单位圆半径R=");
             JTextField Rfield = new JTextField();
             Rfield.setText("1");
             Rfield.setEditable(false);
-            JLabel EA = new JLabel("准确面积=");
+            JLabel EA = new JLabel("单位圆的准确面积(πR^2)=");
             JTextField EAfield = new JTextField("3.1415926535897932");
             EAfield.setEditable(false);
-            JLabel dr = new JLabel("dr=");
+            JLabel dr = new JLabel("dR(R的微分)=R/N=");
             JTextField drfield = new JTextField(String.valueOf(circle.dR));
-            JLabel ex = new JLabel("误差=");
+            drfield.setEditable(false);
+            JLabel ex = new JLabel("相对面积误差=");
             JTextField exfield = new JTextField();
-            Nfield.setText("2147483647");
-            Nfield.addActionListener(e->{
-                double Num = Double.parseDouble(Nfield.getText());
-                if(Num < 900000){
-                    AREA.setText(String.valueOf(circle.setN(Num).AREAR));
-                    exfield.setText(String.valueOf(Double.parseDouble(EAfield.getText()) - Double.parseDouble(AREA.getText())));
-                    drfield.setText(String.valueOf(circle.dR));
-                    circlePanel.repaint();
-                    areaPanel.repaint();
-                }else{
-                    AREA.setText(String.valueOf(circle.setN(Num).AREAR));
-                    exfield.setText(String.valueOf(Double.parseDouble(EAfield.getText()) - Double.parseDouble(AREA.getText())));
-                    drfield.setText(String.valueOf(circle.dR));
-                    JOptionPane.showMessageDialog(this,"你输入的N过大，可能导致渲染时间过长！！！\n但仍可看到计算结果");
+            exfield.setEditable(false);
+            jSlider.addChangeListener(e->{
+                double n = jSlider.getValue();
+                NNt.setText(String.valueOf(jSlider.getValue()));
+                AREA.setText(String.valueOf(circle.setN(n).AREAR));
+                exfield.setText(String.valueOf(Double.parseDouble(EAfield.getText()) - Double.parseDouble(AREA.getText())));
+                drfield.setText(String.valueOf(circle.dR));
+                circlePanel.repaint();
+                areaPanel.repaint();
+            });
+            jSlider.addKeyListener(new KeyListener(){
+                @Override
+                public void keyTyped(KeyEvent e){
+                }
+
+                @Override
+                public void keyPressed(KeyEvent e){
+                    if(e.getKeyCode() == 35) jSlider.setValue(jSlider.getValue() + 1);
+                    if(e.getKeyCode() == 36) jSlider.setValue(jSlider.getValue() - 1);
+                }
+
+                @Override
+                public void keyReleased(KeyEvent e){
                 }
             });
             JMenuBar jMenuBar = new JMenuBar();
             JMenuBar jMenuBar2 = new JMenuBar();
             JMenu jMenu = new JMenu("选项");
             JMenu jMenu2 = new JMenu("关于");
-            JMenuItem jMenuItem1 = new JMenuItem("自动演示");
-            jMenuItem1.addActionListener(e->{
-                for(int i = 1;i<80000;i++){
-                    Nfield.setText(String.valueOf(i));
-                    AREA.setText(String.valueOf(circle.setN(i).AREAR));
-                    exfield.setText(String.valueOf(Double.parseDouble(EAfield.getText()) - Double.parseDouble(AREA.getText())));
-                    drfield.setText(String.valueOf(circle.dR));
-                    circlePanel.repaint();
-                    areaPanel.repaint();
-                }
-            });
             JMenuItem jMenuItem2 = new JMenuItem("退出界面");
             JMenuItem jMenuItem3 = new JMenuItem("作者");
             jMenuItem3.addActionListener(e->JOptionPane.showMessageDialog(this,"软件2211 翟金培"));
             JMenuItem jMenuItem4 = new JMenuItem("小组成员");
-            jMenuItem4.addActionListener(e->{
-                JOptionPane.showMessageDialog(this,"软件2211 翟金培\n软件2211 陈也\n软件2211 殷文喧\n软件2211 仇星\n软件2211 蔡吴江\n软件2211 李方圆");
-            });
+            jMenuItem4.addActionListener(e->JOptionPane.showMessageDialog(this,"软件2211 翟金培\n软件2211 陈也\n软件2211 殷文喧"));
             JMenuItem jMenuItem5 = new JMenuItem("源代码链接");
             jMenuItem5.addActionListener(e->{
                 Desktop desktop = Desktop.getDesktop();
                 try{
-                    //todo:uri
                     URI uri = new URI("https://github.com/40273922/Integrated/tree/master/src/test");
                     desktop.browse(uri);
                 }catch(URISyntaxException | IOException exc){
@@ -129,7 +166,6 @@ public class MainFrame extends JFrame{
                 }
             });
             jMenuItem2.addActionListener(e->System.exit(0));
-            jMenu.add(jMenuItem1);
             jMenu.add(jMenuItem2);
             jMenu2.add(jMenuItem3);
             jMenu2.add(jMenuItem4);
@@ -137,11 +173,15 @@ public class MainFrame extends JFrame{
             jMenu2.add(jMenuItem6);
             jMenuBar.add(jMenu);
             jMenuBar.add(jMenu2);
-            argsPanel.setLayout(new GridLayout(7,2));
+            argsPanel.setLayout(new GridLayout(9,2));
             argsPanel.add(jMenuBar);
             argsPanel.add(jMenuBar2);
             argsPanel.add(N);
-            argsPanel.add(Nfield);
+            argsPanel.add(jSlider);
+            argsPanel.add(tip);
+            argsPanel.add(textField);
+            argsPanel.add(NN);
+            argsPanel.add(NNt);
             argsPanel.add(dr);
             argsPanel.add(drfield);
             argsPanel.add(R);
@@ -152,15 +192,18 @@ public class MainFrame extends JFrame{
             argsPanel.add(AREA);
             argsPanel.add(ex);
             argsPanel.add(exfield);
-            JLabel y = new JLabel("Y:2\u03c0R        ");
+            JLabel y = new JLabel("Y:2πR ");
             JLabel x = new JLabel("X:R");
-            JLabel e = new JLabel("  Y轴最大误差:>-0.003");
+            JLabel e = new JLabel(" Y轴误差<πE-3");
             y.setForeground(Color.red);
             x.setForeground(Color.red);
             e.setForeground(Color.red);
             areaPanel.add(y);
             areaPanel.add(x);
             areaPanel.add(e);
+            argsPanel.setBorder(new TitledBorder(new EtchedBorder(),"参数显示面板"));
+            circlePanel.setBorder(new TitledBorder(new EtchedBorder(),"单位圆对半径进行微分示意图"));
+            areaPanel.setBorder(new TitledBorder(new EtchedBorder(),"微元法积分坐标轴直观图"));
             getContentPane().setLayout(new GridLayout(1,3));
             getContentPane().add(circlePanel);
             getContentPane().add(argsPanel);
@@ -171,3 +214,4 @@ public class MainFrame extends JFrame{
         });
     }
 }
+
